a8247e7bfb1dac27ae7379f6839de62f6972547d
Raf Meeusen <raf.meeusen@telit.com>
2018-09-12 17:24:34 +0200

    completed key generation with pubkey generation and other file formats

--- keygen/openssl_keygen.bash
+++ keygen/openssl_keygen.bash
@@ -3,3 +3,10 @@ printf ""
 
 printf "Generate rsa private key\n"
 openssl genrsa -out rsa_privkey.pem
+openssl rsa -in rsa_privkey.pem -text -out rsa_privkey.txt
+
+printf "Calculate rsa public key from private key\n"
+openssl rsa -in rsa_privkey.pem -pubout -out rsa_pubkey.pem
+openssl rsa -pubin -in rsa_pubkey.pem -text -out rsa_pubkey.txt
+
+
