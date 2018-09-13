4ec0531b8a365770c9aa6537a85a767da4be52b4
Raf Meeusen <raf.meeusen@telit.com>
2018-09-13 10:35:23 +0200

    added ec key gen

--- keygen/openssl_keygen.bash
+++ keygen/openssl_keygen.bash
@@ -10,3 +10,11 @@ openssl rsa -in rsa_privkey.pem -pubout -out rsa_pubkey.pem
 openssl rsa -pubin -in rsa_pubkey.pem -text -out rsa_pubkey.txt
 
 
+printf "Generate ec private key\n"
+openssl genpkey -algorithm ec -pkeyopt ec_paramgen_curve:P-256 -out ./ec_privkey.pem
+openssl ec -in ec_privkey.pem -text -out ec_privkey.txt
+printf "Calculate ec public key from private key\n"
+openssl ec -in ec_privkey.pem -pubout -out ec_pubkey.pem
+openssl ec -pubin -in ec_pubkey.pem -text -out ec_pubkey.txt
+
+
