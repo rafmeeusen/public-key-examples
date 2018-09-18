b2a6ddaf4e8ae9eea9d74e02b6827ff1ad420608
Raf Meeusen <raf.meeusen@telit.com>
2018-09-18 16:24:20 +0200

    rsa signatures: do both PKCS1v1.5 and PSS signatures

--- sign/openssl_sign.bash
+++ sign/openssl_sign.bash
@@ -3,16 +3,44 @@ PUBKEYDIR=../output
 INFILE=../output/inputdata.dgst.sha256.bin
 OUTDIR=../output
 
-printf "Generate signatures, and verify immediately after.\n"
-for algo in rsa ec; do
+function set_vars() {
+    algo=$1
     privkey=$PRIVKEYDIR/privkey.$algo.pem
-    sigfile=$OUTDIR/inputdata.signature.$algo
+    pubkey=$PUBKEYDIR/pubkey.$algo.pem
+}
+
+function print_filenames() {
     echo Using private key file: $privkey
     echo Signing to: $sigfile
-    openssl pkeyutl -sign -in $INFILE -inkey $privkey -out $sigfile
-    pubkey=$PUBKEYDIR/pubkey.$algo.pem
-    echo Verifying with key $pubkey
-    openssl pkeyutl -verify -in $INFILE -pubin -inkey $pubkey -sigfile $sigfile
-done
+}
 
+
+printf "Generate signatures, and verify immediately after.\n"
+set_vars "rsa"
+printf "RSA PSS (contains randomness)\n"
+sigfile=$OUTDIR/inputdata.signature.rsapss
+print_filenames
+rsaoptions="-pkeyopt rsa_padding_mode:pss -pkeyopt digest:sha256"
+# openssl pkeyutl -sign -in ../output/inputdata.dgst.sha256.bin -inkey ../output/privkey.rsa.pem -out ../output/SIGNATURE5.bin -pkeyopt rsa_padding_mode:pss -pkeyopt digest:sha256 -pkeyopt rsa_pss_saltlen:0
+openssl pkeyutl -sign -in $INFILE -inkey $privkey -out $sigfile $rsaoptions
+echo Verifying with key $pubkey
+openssl pkeyutl -verify -in $INFILE -pubin -inkey $pubkey -sigfile $sigfile $rsaoptions
+
+printf "\n"
+printf "RSA PKCS1-v1.5 (deterministic)\n"
+sigfile=$OUTDIR/inputdata.signature.rsapkcs15
+print_filenames
+rsaoptions="-pkeyopt digest:sha256"
+openssl pkeyutl -sign -in $INFILE -inkey $privkey -out $sigfile $rsaoptions
+echo Verifying with key $pubkey
+openssl pkeyutl -verify -in $INFILE -pubin -inkey $pubkey -sigfile $sigfile $rsaoptions
+
+printf "\n"
+set_vars "ec"
+printf "EC\n"
+sigfile=$OUTDIR/inputdata.signature.ec
+print_filenames
+openssl pkeyutl -sign -in $INFILE -inkey $privkey -out $sigfile $rsaoptions
+echo Verifying with key $pubkey
+openssl pkeyutl -verify -in $INFILE -pubin -inkey $pubkey -sigfile $sigfile $rsaoptions
 
