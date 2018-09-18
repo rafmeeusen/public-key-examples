1042d50e7c33b6cc3b387177aa244aa6bf7ed547
Raf Meeusen <raf.meeusen@telit.com>
2018-09-18 14:16:18 +0200

    added openssl signature verification

--- sign/openssl_sign.bash
+++ sign/openssl_sign.bash
@@ -1,14 +1,16 @@
 PRIVKEYDIR=../genkey
 PUBKEYDIR=../genkey
 
-
+printf "Generate signatures, and verify immediately after.\n"
 for algo in rsa ec; do
     privkey=$PRIVKEYDIR/privkey.$algo.pem
     sigfile=inputdata.signature.$algo
     echo Using private key file: $privkey
     echo Signing to: $sigfile
     openssl pkeyutl -sign -in inputdata.txt -inkey $privkey -out $sigfile
+    pubkey=$PUBKEYDIR/pubkey.$algo.pem
+    echo Verifying with key $pubkey
+    openssl pkeyutl -verify -in inputdata.txt -pubin -inkey $pubkey -sigfile $sigfile
 done
-
 
 
