ae60efbb6f84e6fc0d4792b0352f22ff99ac118d
Raf Meeusen <raf.meeusen@telit.com>
2018-09-18 13:45:16 +0200

    using variables for filenames

--- keygen/openssl_keygen.bash
+++ keygen/openssl_keygen.bash
@@ -1,20 +1,31 @@
+
+PRIV_RSA_PEM=privkey.rsa.pem
+PRIV_RSA_TXT=privkey.rsa.txt
+PUB_RSA_PEM=pubkey.rsa.pem
+PUB_RSA_TXT=pubkey.rsa.txt
+
+PRIV_EC_PEM=privkey.ec.pem
+PRIV_EC_TXT=privkey.ec.txt
+PUB_EC_PEM=pubkey.ec.pem
+PUB_EC_TXT=pubkey.ec.txt
+
+
 printf "Openssl command line examples\n"
 printf ""
 
 printf "Generate rsa private key\n"
-openssl genpkey -algorithm rsa -pkeyopt rsa_keygen_bits:2048 -out rsa_privkey.pem
-openssl rsa -in rsa_privkey.pem -text -out rsa_privkey.txt
-
+openssl genpkey -algorithm rsa -pkeyopt rsa_keygen_bits:2048 -out $PRIV_RSA_PEM
+openssl rsa -in $PRIV_RSA_PEM -text -out $PRIV_RSA_TXT
 printf "Calculate rsa public key from private key\n"
-openssl rsa -in rsa_privkey.pem -pubout -out rsa_pubkey.pem
-openssl rsa -pubin -in rsa_pubkey.pem -text -out rsa_pubkey.txt
+openssl rsa -in $PRIV_RSA_PEM -pubout -out $PUB_RSA_PEM
+openssl rsa -pubin -in $PUB_RSA_PEM -text -out $PUB_RSA_TXT
 
 
 printf "Generate ec private key\n"
-openssl genpkey -algorithm ec -pkeyopt ec_paramgen_curve:P-256 -out ./ec_privkey.pem
-openssl ec -in ec_privkey.pem -text -out ec_privkey.txt
+openssl genpkey -algorithm ec -pkeyopt ec_paramgen_curve:P-256 -out $PRIV_EC_PEM
+openssl ec -in $PRIV_EC_PEM -text -out $PRIV_EC_TXT
 printf "Calculate ec public key from private key\n"
-openssl ec -in ec_privkey.pem -pubout -out ec_pubkey.pem
-openssl ec -pubin -in ec_pubkey.pem -text -out ec_pubkey.txt
+openssl ec -in $PRIV_EC_PEM -pubout -out $PUB_EC_PEM
+openssl ec -pubin -in $PUB_EC_PEM -text -out $PUB_EC_TXT
 
 
