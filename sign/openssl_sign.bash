3e38649014dd98e792d5e9cfd0a7a3335f08121f
Raf Meeusen <raf.meeusen@telit.com>
2018-09-18 15:29:29 +0200

    fixed broken sign operations after dir shuffling

--- sign/openssl_sign.bash
+++ sign/openssl_sign.bash
@@ -1,16 +1,18 @@
-PRIVKEYDIR=../genkey
-PUBKEYDIR=../genkey
+PRIVKEYDIR=../output
+PUBKEYDIR=../output
+INFILE=../output/inputdata.dgst.sha256.bin
+OUTDIR=../output
 
 printf "Generate signatures, and verify immediately after.\n"
 for algo in rsa ec; do
     privkey=$PRIVKEYDIR/privkey.$algo.pem
-    sigfile=inputdata.signature.$algo
+    sigfile=$OUTDIR/inputdata.signature.$algo
     echo Using private key file: $privkey
     echo Signing to: $sigfile
-    openssl pkeyutl -sign -in inputdata.txt -inkey $privkey -out $sigfile
+    openssl pkeyutl -sign -in $INFILE -inkey $privkey -out $sigfile
     pubkey=$PUBKEYDIR/pubkey.$algo.pem
     echo Verifying with key $pubkey
-    openssl pkeyutl -verify -in inputdata.txt -pubin -inkey $pubkey -sigfile $sigfile
+    openssl pkeyutl -verify -in $INFILE -pubin -inkey $pubkey -sigfile $sigfile
 done
 
 
