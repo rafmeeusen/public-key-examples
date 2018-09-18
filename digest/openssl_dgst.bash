1107a7b5ea6a6e34a954dee6d324d73bb2198290
Raf Meeusen <raf.meeusen@telit.com>
2018-09-18 15:22:30 +0200

    dgst generation to output dir

--- digest/openssl_dgst.bash
+++ digest/openssl_dgst.bash
@@ -1,6 +1,7 @@
 INFILE=../data/inputdata.txt
-OUTBIN=inputdata.dgst.sha256.bin
-OUTTXT=inputdata.dgst.sha256.txt
+OUTDIR=../output
+OUTBIN=$OUTDIR/inputdata.dgst.sha256.bin
+OUTTXT=$OUTDIR/inputdata.dgst.sha256.txt
 
 openssl dgst -sha256 -binary -out $OUTBIN $INFILE
 openssl dgst -sha256 -out $OUTTXT $INFILE
