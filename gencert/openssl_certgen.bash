6d329af716e8c8350eff937ba923a40216bdd885
Raf Meeusen <raf.meeusen@telit.com>
2018-11-12 14:17:24 +0100

    just comment fixed

--- gencert/openssl_certgen.bash
+++ gencert/openssl_certgen.bash
@@ -4,6 +4,6 @@ KEYDIR=../output
 CERT=$OUTDIR/root.rsa.pem
 KEY=$KEYDIR/privkey.rsa.pem
 
-printf "Generating $CERT.\n"
+printf "Generating self-signed $CERT.\n"
 openssl req -x509 -key $KEY -config ../openssl_cfg/rootca.cfg -outform PEM -out $CERT -set_serial 0
 openssl x509 -in $CERT -text -out $CERT.txt
