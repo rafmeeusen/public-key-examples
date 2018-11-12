590683c2f45cba82149eec56aa351215162bbe7e
Raf Meeusen <raf.meeusen@telit.com>
2018-11-12 14:07:31 +0100

    serial of root CA to 0 as most CAs seem to do

--- gencert/openssl_certgen.bash
+++ gencert/openssl_certgen.bash
@@ -5,5 +5,5 @@ CERT=$OUTDIR/root.rsa.pem
 KEY=$KEYDIR/privkey.rsa.pem
 
 printf "Generating $CERT.\n"
-openssl req -x509 -key $KEY -config ../openssl_cfg/rootca.cfg -outform PEM -out $CERT
+openssl req -x509 -key $KEY -config ../openssl_cfg/rootca.cfg -outform PEM -out $CERT -set_serial 0
 openssl x509 -in $CERT -text -out $CERT.txt
